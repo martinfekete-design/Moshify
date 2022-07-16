3e9635bbb89b3d6459e676d687b05d7432133809
martin-fekete <108697020+martin-fekte@users.noreply.github.com>
2022-07-16 16:43:15 +0200

    DIST

---
+++ dist/index.1fcb0ae9.js
@@ -0,0 +1,2 @@
+const collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach((l=>l.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
+//# sourceMappingURL=index.1fcb0ae9.js.map
